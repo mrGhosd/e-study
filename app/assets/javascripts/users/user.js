f2af36d6fc36999086bacf524ff51940cf701d41
vsokoltsov <vforvad@gmail.com>
2015-09-24 17:55:30 +0300

    add registration date to user list item

--- app/assets/javascripts/users/user.js
+++ app/assets/javascripts/users/user.js
@@ -5,10 +5,20 @@ var User = function(attributes){
 
     this.correctNaming = function(){
         if(this.surname && this.name){
+            if(this.secondname){
+                return this.surname + " " + this.name + " " + this.secondname;
+            }
             return this.surname + " " + this.name;
         } else {
             return this.email;
         }
+    };
+
+    this.humanizedDate = function(date){
+        var newDate = new Date(date);
+        var time = [newDate.getHours(), newDate.getMinutes(), newDate.getSeconds()];
+        var date = [newDate.getDate(), newDate.getMonth() + 1, newDate.getFullYear()];
+        return time.join(":") + " " + date.join(".");
     };
     return this;
 };
