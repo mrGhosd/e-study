f639db1e2a8ce1c7f9179894b4764be1a211ea59
vsokoltsov <vforvad@gmail.com>
2015-12-11 00:25:21 +0300

    remove useless migrations

--- db/migrate/20150927165750_add_private_column_to_chat.rb
+++
@@ -1,5 +0,0 @@
-class AddPrivateColumnToChat < ActiveRecord::Migration
-  def change
-    add_column :chats, :private, :boolean, default: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150927165750) do
+ActiveRecord::Schema.define(version: 20150926080818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,17 +28,6 @@ ActiveRecord::Schema.define(version: 20150927165750) do
   add_index "authorizations", ["uid"], name: "index_authorizations_on_uid", using: :btree
   add_index "authorizations", ["user_id"], name: "index_authorizations_on_user_id", using: :btree
 
-  create_table "chats", force: :cascade do |t|
-    t.integer  "owner_id",                  null: false
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "private",    default: true
-  end
-
-  add_index "chats", ["name"], name: "index_chats_on_name", using: :btree
-  add_index "chats", ["owner_id"], name: "index_chats_on_owner_id", using: :btree
-
   create_table "images", force: :cascade do |t|
     t.integer  "imageable_id"
     t.string   "imageable_type"
@@ -49,27 +38,6 @@ ActiveRecord::Schema.define(version: 20150927165750) do
 
   add_index "images", ["imageable_id"], name: "index_images_on_imageable_id", using: :btree
   add_index "images", ["imageable_type"], name: "index_images_on_imageable_type", using: :btree
-
-  create_table "messages", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "chat_id",    null: false
-    t.text     "text"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "messages", ["chat_id"], name: "index_messages_on_chat_id", using: :btree
-  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
-
-  create_table "user_chats", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "chat_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "user_chats", ["chat_id"], name: "index_user_chats_on_chat_id", using: :btree
-  add_index "user_chats", ["user_id"], name: "index_user_chats_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
@@ -88,5 +56,4 @@ ActiveRecord::Schema.define(version: 20150927165750) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["password_digest"], name: "index_users_on_password_digest", using: :btree
 
-  add_foreign_key "chats", "users", column: "owner_id"
 end
