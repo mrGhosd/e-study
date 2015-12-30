c783ca5447cba07aaf0c3baca8b52b8173badb69
vsokoltsov <vforvad@gmail.com>
2015-12-31 00:20:44 +0300

    add actions for creation message without chat_id

--- app/models/form/message.rb
+++ app/models/form/message.rb
@@ -8,6 +8,7 @@ class Form::Message < Form::Base
 
   def attributes=(attrs)
     super
+    find_or_create_chat(attrs) if @object.chat.blank?
     @attaches = attrs["attaches"]
   end
 
@@ -17,7 +18,7 @@ class Form::Message < Form::Base
         Attach.find_by(id: attach["data"]["id"], attachable_type: attach["data"]["attachable_type"])
               .update(attachable_id: @object.id)
       end if attaches.present?
-      $redis.publish 'rtchange', MessageSerializer.new(@object).serializable_hash.to_json
+      $redis.publish 'rtchange', {id: @object.chat.id, obj: MessageSerializer.new(@object).serializable_hash.to_json }.to_json
     end
   end
 
@@ -26,5 +27,18 @@ class Form::Message < Form::Base
       self.errors.add(:text, "Message text can't be empty")
       false
     end
+  end
+
+  def find_or_create_chat(attrs)
+    users = attrs["users"].join(", ")
+    finded_chats = UserChat.in_same_chat(users)
+    if finded_chats.present?
+      chat_id = finded_chats.map(&:chat_id).uniq.first
+    else
+      chat = Chat.create!
+      attrs["users"].each { |u| chat.users << User.find(u) }
+      chat_id = chat.id
+    end
+    self.chat_id = chat_id
   end
 end
--- app/models/user_chat.rb
+++ app/models/user_chat.rb
@@ -3,5 +3,6 @@ class UserChat < ActiveRecord::Base
   belongs_to :chat
 
   scope :in_same_chat, -> (users) { find_by_sql("select * from user_chats where
-  chat_id in (select chat_id from user_chats group by chat_id having count(*) > 1) and user_id in (#{users});") }
+  chat_id in (select chat_id from user_chats group by chat_id having count(*) > 1)
+  and user_id in (#{users});") }
 end
