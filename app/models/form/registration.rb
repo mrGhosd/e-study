30058369362f4d34bb7ac3d8f1366c354a132b45
Vadim Sokoltsov <vforvad@gmail.com>
2016-03-07 23:46:40 +0300

    rename form-object specs

--- app/models/form/registration.rb
+++ app/models/form/registration.rb
@@ -1,13 +1,13 @@
 class Form::Registration < Form::Base
+  include PasswordValidation
+  include EmailValidation
   include JsonWebToken
+  
   attr_accessor :token
   attribute :email
   attribute :password
   attribute :password_confirmation
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence:   true, format:     { with: VALID_EMAIL_REGEX }
-  validates :password, length: { minimum: 6 }
   validates_confirmation_of :password, if: lambda { |m| m.password.present? }
 
   def email=(attr)
