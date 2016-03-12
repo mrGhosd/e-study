b292387e0350d920129a7c308ffbf663a7b269a2
Vadim Sokoltsov <vforvad@gmail.com>
2016-03-12 11:32:53 +0300

    add before_action hooks for online and locale

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -3,4 +3,16 @@ class Api::ApiController < ApplicationController
   include SessionConcern
   respond_to :json
 
+  before_action :update_last_sign_in_time, if: -> { current_user.present? }
+  before_action :set_locale
+
+  private
+
+  def update_last_sign_in_time
+    current_user.update!(last_sign_in_at: Time.zone.now)
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
