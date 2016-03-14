99b69ebc398d53e3d3fd30901499827200f8f949
Vadim Sokoltsov <vforvad@gmail.com>
2016-03-15 02:27:28 +0300

    add search for countries model

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,17 @@
+require 'elasticsearch/model'
+
 class Country < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 end
+
+# Delete the previous articles index in Elasticsearch
+Country.__elasticsearch__.client.indices.delete index: Country.index_name
+
+# Create the new index with the new mapping
+Country.__elasticsearch__.client.indices.create index: Country.index_name, body:
+                                                {
+                                                  settings: Country.settings.to_hash,
+                                                  mappings: Country.mappings.to_hash
+                                                }
+Country.import
