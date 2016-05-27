e7f4a4ce1e7cc1ac63c5915dcf1d018baf94610a
Vadim Sokoltsov <vforvad@gmail.com>
2016-05-27 12:43:46 +0300

    add show and update routes

--- app/controllers/api/v0/courses_controller.rb
+++ app/controllers/api/v0/courses_controller.rb
@@ -14,4 +14,19 @@ class Api::V0::CoursesController < Api::ApiController
       render json: { errors: form.errors }, status: :unprocessable_entity
     end
   end
+
+  def show
+    course = current_user.courses.find(params[:id])
+    render json: { course: course }
+  end
+
+  def update
+    course = current_user.courses.find(params[:id])
+    form = Form::Course.new(course, params[:course])
+    if form.submit
+      render json: { course: form.object }
+    else
+      render json: { errors: form.errors }, status: :unprocessable_entity
+    end
+  end
 end
