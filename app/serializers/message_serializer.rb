01a82402b39fa7427fe8c100a2f6f61ecfafd5e7
Vadim Sokoltsov <vforvad@gmail.com>
2016-05-29 19:19:52 +0300

    delete chat and messages serializers

--- app/serializers/message_serializer.rb
+++
@@ -1,7 +0,0 @@
-class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :text, :chat_id, :created_at
-
-  has_one :user
-  has_one :chat
-  has_many :attaches, serializer: AttachSerializer
-end
