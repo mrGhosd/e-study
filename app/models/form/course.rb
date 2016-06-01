21ff730f3e5392abb475bd71404be8ade6808779
Vadim Sokoltsov <vforvad@gmail.com>
2016-06-01 03:12:54 +0300

    add lessons controller and destroy action

--- app/models/form/course.rb
+++ app/models/form/course.rb
@@ -1,6 +1,43 @@
 class Form::Course < Form::Base
+  attr_accessor :lessons
+
   attribute :title
   attribute :description
 
   validates :title, :description, presence: true
+  validates :lessons, length: { minimum: 1 }
+
+  def attributes=(attributes)
+    super(attributes)
+    @lessons = attributes['lessons']
+  end
+
+  def submit
+    super do
+      @lessons.each_with_index do |attr, index|
+        lesson = attr.has_key?('id') ? find_lesson(attr['id']) : build_new_lesson
+        form = Form::Lesson.new(lesson, attr)
+        unless form.submit
+          add_errors(form.errors, index)
+          false
+        end
+      end if @lessons.present?
+      errors.blank?
+    end
+  end
+
+  private
+
+  def build_new_lesson
+    @object.lessons.build(user_id: @object.user_id)
+  end
+
+  def find_lesson(id)
+    @object.lessons.find(id)
+  end
+
+  def add_errors(error, index)
+    errors.add(:lessons, { index => error.messages })
+    false
+  end
 end
