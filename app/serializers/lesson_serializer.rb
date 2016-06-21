7c5e8ba4d5580ec5557aff1e9dac15648a653fda
Vadim Sokoltsov <vforvad@gmail.com>
2016-06-21 21:46:25 +0300

    add lessons validation for current_user

--- app/serializers/lesson_serializer.rb
+++ app/serializers/lesson_serializer.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 class LessonSerializer < ActiveModel::Serializer
   root 'lesson'
-  attributes :id, :title, :description, :slug, :created_at, :user_id, :course
+  attributes :id, :title, :description, :slug, :created_at, :user_id, :course,
+             :homeworks
 
   has_many :comments
-  has_many :homeworks
 
   delegate :course, to: :object
+
+  def homeworks
+    object.homeworks.where(user_id: scope.id)
+  end
 end
