31559bf5d446dcaf07e9142cdcc9f7d7c8a54909
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-22 00:45:24 +0300

    change serializer

--- app/serializers/courses_serializer.rb
+++ app/serializers/courses_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class CoursesSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :short_description, :slug, :created_at, :user_id
+  attributes :id, :title, :description, :short_description, :slug, :difficult, :created_at, :user_id
 
   has_one :image, serializer: AttachSerializer
 end
