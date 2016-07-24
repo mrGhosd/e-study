d2243d0837a0517f5dc64e0c3d04716249691bcd
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-24 23:10:33 +0300

    add some changes to serializers

--- app/serializers/homeworks_serializer.rb
+++ app/serializers/homeworks_serializer.rb
@@ -3,8 +3,8 @@ class HomeworkSerializer < ActiveModel::Serializer
   root 'homework'
   attributes :id, :text, :created_at, :user_id, :lesson, :course
 
-  has_many :comments
   has_one :user, serializer: UserSerializer
+
   delegate :lesson, to: :object
 
   def course
--- app/serializers/lesson_serializer.rb
+++ app/serializers/lesson_serializer.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 class LessonSerializer < ActiveModel::Serializer
   root 'lesson'
-  attributes :id, :title, :description, :slug, :created_at, :user_id, :course,
-             :homeworks
+  attributes :id, :title, :description, :slug, :created_at, :user_id, :course
 
   has_many :comments
+  has_one :user, serializer: UserSerializer
+  has_many :homeworks, serializer: HomeworkSerializer
 
   delegate :course, to: :object
-
-  def homeworks
-    scope.present? ? object.homeworks.where(user_id: scope.id) : []
-  end
 end
