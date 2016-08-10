f6c1f7285750dec0886a10aff56eac52266685e0
Vadim Sokoltsov <vforvad@gmail.com>
2016-08-10 23:56:25 +0300

    remove useless actions

--- app/models/form/search.rb
+++ app/models/form/search.rb
@@ -13,22 +13,4 @@ class Form::Search < Form::Base
       object.camelize.constantize.limit(DEFAULT_LIMIT_SIZE)
     end
   end
-
-  def search_chats
-    scope = object.chats
-    if query.present?
-      scope.search("*#{query}*").records.to_a
-    else
-      scope.limit(DEFAULT_CHAT_LIMIT)
-    end
-  end
-
-  def search_messages
-    scope = object.messages
-    if query.present?
-      scope.search("*#{query}*").records.to_a
-    else
-      scope.limit(DEFAULT_MESSAGES_LIMIT)
-    end
-  end
 end
