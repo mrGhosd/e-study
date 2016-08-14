b689a584216d5020383f4c12145e77199d6d2c0a
Vadim Sokoltsov <vforvad@gmail.com>
2016-08-14 23:59:16 +0300

    change lesson policy

--- app/policies/lesson_policy.rb
+++ app/policies/lesson_policy.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 class LessonPolicy < ApplicationPolicy
+  def show?
+    record.user == user ||
+      record.teacher_id == user.id ||
+      record.course.students.include?(user)
+  end
+
   def destroy?
     record.user == user
   end
