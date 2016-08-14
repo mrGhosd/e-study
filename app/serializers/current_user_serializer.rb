5328ff7852d076dda4c7db3508634ae1c97e0c71
Vadim Sokoltsov <vforvad@gmail.com>
2016-08-14 23:12:05 +0300

    change current user serializer

--- app/serializers/current_user_serializer.rb
+++ app/serializers/current_user_serializer.rb
@@ -3,12 +3,16 @@ class CurrentUserSerializer < ActiveModel::Serializer
   include DateConcern
   root 'current_user'
   attributes :id, :last_name, :first_name, :email, :middle_name, :notifications,
-             :last_sign_in_at, :date_of_birth, :date_of_birth_h
+             :last_sign_in_at, :date_of_birth, :date_of_birth_h, :studying_courses
 
   has_one :image, serializer: AttachSerializer
   has_one :background_image, serializer: AttachSerializer
 
   def notifications
     object.notifications.active || []
+  end
+
+  def studying_courses
+    User.last.studying_courses.map(&:id)
   end
 end
