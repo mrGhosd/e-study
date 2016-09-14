af41604ab3b68b91ce0315310b22c650aaee5209
vsokoltsov <vforvad@gmail.com>
2016-09-14 13:39:52 +0300

    return serializer

--- app/serializers/users_serializer.rb
+++ app/serializers/users_serializer.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
-# class UsersSerializer < ActiveModel::Serializer
-#   attributes :id, :last_name, :first_name, :email, :middle_name, :created_at, :last_sign_in_at
-#
-#   has_one :image, serializer: AttachSerializer
-#
-#   def image
-#     object.image if object.present?
-#   end
-# end
+class UsersSerializer < ActiveModel::Serializer
+  attributes :id, :last_name, :first_name, :email, :middle_name, :created_at, :last_sign_in_at
+
+  has_one :image, serializer: AttachSerializer
+
+  def image
+    object.image if object.present?
+  end
+end
