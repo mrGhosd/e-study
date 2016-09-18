32029f2489f2f41b86296239eeaa252f13b38fa2
vsokoltsov <vforvad@gmail.com>
2016-09-19 01:02:42 +0300

    update lesson worker for updating an repeated lessons

--- app/workers/lesson_worker.rb
+++ app/workers/lesson_worker.rb
@@ -6,6 +6,19 @@ class LessonWorker
 
   def perform
     Lesson.where('end_date < ?', Time.zone.now).update_all(active: false)
+    update_repeated_lessons
     # Add support of updated_lessons
+  end
+
+  private
+
+  def update_repeated_lessons
+    ActiveRecord::Base.transaction do
+      Lesson.where(repeated: true, active: false).each do |lesson|
+        begin_date = lesson.begin_date + lesson.period.days
+        end_date = lesson.end_date + lesson.period.days
+        lesson.update!(begin_date: begin_date, end_date: end_date)
+      end
+    end
   end
 end
